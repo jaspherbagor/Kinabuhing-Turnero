a594bc84408d6367e898bff54b1967ed7743e2cb
Jaspher Bagor <jaspherbagor0357@gmail.com>
2023-10-03 16:04:21 +0800

    added margins to the elements in the js

--- scroll-reveal-animation.js
+++ scroll-reveal-animation.js
@@ -6,30 +6,50 @@ const sr = ScrollReveal ({
 });
 
 // Home Page
-sr.reveal('.hero-text, .service-quality-text, .services-items-section h2, .clients-reviews-section h2', {origin:'top'});
+sr.reveal(
+    '.hero-text, .service-quality-text, .services-items-section h2, .clients-reviews-section h2',
 
-sr.reveal('.services-quality-items:nth-child(2), .services-items:nth-child(5), .carousel-item',{origin:'bottom'});
+    {origin:'top'}
 
-sr.reveal('.services-quality-items:nth-child(1), .services-items:nth-child(1), .services-items:nth-child(3), .contact-info-left', {origin:'left'});
+);
 
-sr.reveal('.services-quality-items:nth-child(3), .services-items:nth-child(2), .services-items:nth-child(4), .contact-map-right', {origin:'right'});
+sr.reveal(
+    '.services-quality-items:nth-child(2), .services-items:nth-child(5), .carousel-item',
+
+    {origin:'bottom'}
+
+);
+
+sr.reveal(
+    '.services-quality-items:nth-child(1), .services-items:nth-child(1), .services-items:nth-child(3), .contact-info-left', 
+    
+    {origin:'left'}
+    
+);
+
+sr.reveal(
+    '.services-quality-items:nth-child(3), .services-items:nth-child(2), .services-items:nth-child(4), .contact-map-right',
+
+    {origin:'right'}
+
+);
 
 // About Us Page
 sr.reveal(
     '.how-we-operate-section h2, .company-history-head, .work-team-section h2, .mission-and-values-head, .mission-div, .core-values-head ',
 
-     {origin:'top'}
+    {origin:'top'}
 );
 
 sr.reveal(
     'div.how-we-operate-text, .company-history-row-left:nth-child(1), .company-history-row-right:nth-child(1), .work-team-items:nth-child(1), .work-team-items:nth-child(3), .core-values-left ',
 
-     {origin:'left'}
+    {origin:'left'}
 );
 
 sr.reveal(
     'div.how-we-operate-img, .company-history-row-right:nth-child(2), .company-history-row-left:nth-child(2), .work-team-items:nth-child(2), .work-team-items:nth-child(4), .core-values-right',
 
-     {origin:'right'}  
+    {origin:'right'}  
 );
 
