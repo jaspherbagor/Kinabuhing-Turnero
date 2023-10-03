93436f321a42430f55399b30013f1dabd247f57a
Jaspher Bagor <jaspherbagor0357@gmail.com>
2023-10-03 08:22:46 +0800

    added scroll reveal on the home page

--- scroll-reveal-animation.js
+++ scroll-reveal-animation.js
@@ -1,13 +1,16 @@
 const sr = ScrollReveal ({
     distance: '40px',
-    duration:1050,
-    delay:100,
+    duration:1250,
+    delay:150,
     reset:true
 });
 
-sr.reveal('.hero-text', {origin:'top'});
-sr.reveal('',{origin:'bottom'});
-sr.reveal('',{origin:'top'});
-sr.reveal('', {origin:'left'});
-sr.reveal('', {origin:'right'});
-sr.reveal('', {origin:'top'});
+// Home Page Start
+sr.reveal('.hero-text, .service-quality-text, .services-items-section h2, .clients-reviews-section h2', {origin:'top'});
+
+sr.reveal('.services-quality-items:nth-child(2), .services-items:nth-child(5), .carousel-item',{origin:'bottom'});
+
+sr.reveal('.services-quality-items:nth-child(1), .services-items:nth-child(1), .services-items:nth-child(3), .contact-info-left', {origin:'left'});
+
+sr.reveal('.services-quality-items:nth-child(3), .services-items:nth-child(2), .services-items:nth-child(4), .contact-map-right', {origin:'right'});
+// Home Page End
